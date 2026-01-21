9af331413b02c2f7e9c0d3fd7e09c2ddafe92c0b
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-01-21 11:33:04 +0800

    11.2 Задачи базового уровня. Задача 1.

--- 10 Коллекции/Список/Program.cs
+++ 10 Коллекции/Список/Program.cs
@@ -5,6 +5,19 @@ namespace Список
     {
         static void Main(string[] args)
         {
+            Task_base_11_2_1(); //Заполни список
+        }
+
+
+        static void Task_base_11_2_1() //Заполни список
+        {
+            int n = int.TryParse(Console.ReadLine(), out n)?n:0; //количество элементов в списке
+            List<string> strings = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                strings.Add(Console.ReadLine());
+            }
+            Console.WriteLine(string.Join(" ",strings));
         }
     }
 }
